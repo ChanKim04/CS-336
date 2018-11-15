cae2a341777e20d069856d7013063dc068e6bfde
Chan Kim <kimchan04@gmail.com>
2018-11-15 01:15:00 -0500

    Lab11

---
+++ app/scripts/CommentList.js
@@ -0,0 +1,25 @@
+// Lab09
+
+// created Fall 2018 (11/02) 
+// @author: Chan Kim (ck45) for CS 336 at Calvin College 
+
+import React from 'react';
+
+import Comment from './Comment';
+
+module.exports = React.createClass({
+    render: function() {
+      var commentNodes = this.props.data.map(function(comment) {
+        return (
+          <Comment author={comment.author} key={comment.id}>
+            {comment.text}
+          </Comment>
+        );
+      });
+      return (
+        <div className="commentList">
+          {commentNodes}
+        </div>
+      );
+    }
+  });
